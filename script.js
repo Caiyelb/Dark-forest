2061912ff8bbec141f28c06ffd3bfdbb6e31b4fa
Caiyelb <144827338+Caiyelb@users.noreply.github.com>
2024-04-02 12:01:08 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,17 @@
+$(".end").hide();
+$(".path-one").click(function() {
+ $(".message-1").show();
+
+});
+
+$(".path-two").click(function() {
+    $(".").hide();
+});
+
+$(".path-three").click(function() {
+    $(".").hide();
+});
+
+$(".path-4").click(function() {
+    $(".").hide();
+});
